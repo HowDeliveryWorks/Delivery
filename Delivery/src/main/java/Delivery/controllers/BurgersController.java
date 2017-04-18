0df5803267b80285e8263bfd00268b6556e829b8
igorIsyutin <igor@iMac-Igor.local>
2017-04-18 19:19:10 +0300

    ControllerWorks
    Now we can see our pages ;)
    To run ./gradlew bootRun

--- Delivery/src/main/java/Delivery/controllers/BurgersController.java
+++ Delivery/src/main/java/Delivery/controllers/BurgersController.java
@@ -4,6 +4,7 @@ import Delivery.DAO.BurgersDAO;
 import Delivery.model.Burger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +13,24 @@ import java.util.List;
 /**
  * Created by igor on 08.04.17.
  */
-@RestController
+@Controller
 public class BurgersController {
 
     @Autowired
     private BurgersDAO dao;
 
     @GetMapping("/menu")
-    public String getAllBurgers(){
-        return dao.findAll().toString();
+    public String getAllBurgers(Model model){
+        return "/web/store";
+    }
+
+    @GetMapping("/cart")
+    public String cart(Model model){
+        return "/web/cart";
+    }
+
+    @GetMapping("/cart2")
+    public String cart2(Model model){
+        return "/web/cart2";
     }
 }
---
+++ Delivery/src/main/java/Delivery/controllers/WelcomeController.java
@@ -0,0 +1,25 @@
+package Delivery.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by igor on 18.04.17.
+ */
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "/web/hello";
+    }
+
+    @GetMapping("/store")
+    public String store(Model model) {
+        return "/web/store";
+    }
+}
